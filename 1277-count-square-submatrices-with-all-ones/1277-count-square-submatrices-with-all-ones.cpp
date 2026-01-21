163b469a9ac678c1e902a992707b734039c36865
Chirag Sharma <chirag.k24@iiits.in>
2026-01-21 19:37:11 +0530

    Time: 10 ms (34.54%), Space: 30.4 MB (20.22%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -9,11 +9,9 @@ public:
 
         for (int j = 0; j < n; j++){
             dp[0][j] = matrix[0][j];
-            sum+=dp[0][j];
         }
         for (int i = 0; i < m; i++){
             dp[i][0] = matrix[i][0];
-            sum+=dp[i][0];
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
@@ -22,10 +20,15 @@ public:
                 else {
                     dp[i][j] = 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
                 }
-                sum+= dp[i][j];
             }
         }
         
-        return sum-dp[0][0];
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                sum+=dp[i][j];
+            }
+        }
+
+        return sum;
     }
 };
