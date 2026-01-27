74cfe3992decfc795e6ff8116fb4496167a8f0fe
Chirag Sharma <chirag.k24@iiits.in>
2026-01-27 18:40:38 +0530

    Time: 83 ms (48.1%), Space: 112.8 MB (87.45%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         int m = isWater.size(), n = isWater[0].size();
-        vector<vector<int>> height(m, vector<int>(n));
-
         queue<pair<int, int>> q;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                height[i][j] = -1;
                 if (isWater[i][j] == 1) {
-                    height[i][j] = 0;
                     q.push({i, j});
                 }
+                // else isWater[i][j] = 0;
             }
         }
 
@@ -22,12 +19,18 @@ public:
             for (int i = 0; i < 4; i++) {
                 int row = r + delr[i];
                 int col = c + delc[i];
-                if(row >= 0 && row < m && col>= 0 && col < n && isWater[row][col] == 0 && height[row][col] == -1 ){
-                    height[row][col] = height[r][c] + 1;
-                    q.push({row,col});
+                if (row >= 0 && row < m && col >= 0 && col < n &&
+                    isWater[row][col] == 0) {
+                    isWater[row][col] = isWater[r][c] + 1;
+                    q.push({row, col});
                 }
             }
         }
-        return height;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                isWater[i][j]--;
+            }
+        }
+        return isWater;
     }
 };
